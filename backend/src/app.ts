2757e9e8dc10a9f434f571d5216f3461f20eb7af
Axel1208 <juandavidospinagutierrez44gmail.com>
2026-08-16 19:29:09 -0500

    back front

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import uploadRoutes from './routes/upload.routes'
 import productRoutes from './routes/product.routes'
 const app = express()
-
+//comentariosss
 app.use(express.json())
 
 app.use('/api/upload', uploadRoutes)
